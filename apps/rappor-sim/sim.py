f45fe71a6bffcfb6449d62b492f73b27754595e4
Killian <davittk@tcd.ie>
2022-08-04 15:25:32 +0100

    Made sim not delete map after changes to threshold

--- apps/rappor-sim/sim.py
+++ apps/rappor-sim/sim.py
@@ -1,8 +1,14 @@
 import subprocess
 import os
 
-def run_sim(params, threshold):
+def delete_map():
+    try:
+        os.remove('map.Rdata')
+    except FileNotFoundError: 
+        print("Error: couldn't remove the map file, sim probabily failed")
 
+
+def write_params(params):
     # Write params to file
     with open('params.csv', 'r') as file:
         data = file.readlines()
@@ -12,23 +18,25 @@ def run_sim(params, threshold):
         file.writelines(data)
         file.write('\n')
 
+def write_decode_params(decode):
     with open('decode.csv', 'r') as file:
         data = file.readlines()
 
-    data[1] = str(threshold)
+    data[1] = str(decode) + ",majority, majority"
     with open('decode.csv', 'w') as file:
         file.writelines(data)
         file.write('\n')
 
+def run_sim(params, threshold):
+
+    write_params(params)
+    write_decode_params(threshold) 
     new_env = os.environ.copy()
     new_env["RAPPOR_REPO"] = "../../"
     bashCommand = "Rscript cli.R map.Rdata params.csv pop_params.csv decode.csv"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, env=new_env)
     output, error = process.communicate()
-    try:
-        os.remove('map.Rdata')
-    except FileNotFoundError: 
-        print("Error: couldn't remove the map file, sim probabily failed")
+
     results = output.decode('utf').split('\n')
     results = [x.split(',')[1:] for x in results]
     results_check = results[6:8]
@@ -44,14 +52,15 @@ sim_results = []
 
 params = [2048,2,8,0.5,0.75,0]
 thresholds = [0.002, 0.005, 0.001]
-filter_sizes = range(0,14)
-filter_sizes = [2**x for x in filter_sizes[12:]]
+filter_sizes = range(0,2)
+filter_sizes = [2**x for x in filter_sizes]
 cohort_sizes = [256]#,64,128,256,512,1024,2048,4096]
 
 
 for filter_size in filter_sizes:
     params[0]=filter_size
     for size in cohort_sizes:
+        delete_map()
         for t in thresholds:
             params[2] = size
             sim_results.append((run_sim(params, t), params.copy()))
