41cb907c495060758cf4fe2ce8342f9ed3d820d8
Killian <davittk@tcd.ie>
2022-09-09 16:14:52 +0100

    Fixed putting parameters into separate python files

--- apps/rappor-sim/simulation_params.py
+++ apps/rappor-sim/simulation_params.py
@@ -1,5 +1,9 @@
-filter_sizes = [256,1024,4096]
+filter_sizes = [2**x for x in range(8,16)]
 numbers_of_cohorts = [2]
-threshold_values = [0.001, 0.002, 0.003]
+threshold_values = [0.00005,0.0001,0.0005,0.001,0.005, 0.01, 0.05, 0.1,0.5]
+#threshold_values = [0.0001]
 primary_decisions = ['majority', 'any', 'all']
-secondary_decisions = ['majority','any','all']
+secondary_decisions = ['any','all']
+
+populations = [300]
+samples = [30000]
