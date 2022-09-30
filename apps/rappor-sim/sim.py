3a2233a501ce2ae3f1c383bf297c8571eb60a220
Killian <davittk@tcd.ie>
2022-09-30 18:34:38 +0100

    Lots of tidying of the sim.py file for clarity

--- apps/rappor-sim/sim.py
+++ apps/rappor-sim/sim.py
@@ -1,21 +1,26 @@
 import subprocess
-import os, sys
-from simulation_params import numbers_of_cohorts, filter_sizes, primary_decisions, secondary_decisions, primary_threshold_values, secondary_threshold_values, populations, samples, num_jobs
+import os
+import sys
 import itertools
 import numpy as np
+
+# Importing the range of parameters from other python file
+from simulation_params import (numbers_of_cohorts, filter_sizes, primary_decisions, secondary_decisions, primary_threshold_values, secondary_threshold_values, populations, samples, num_jobs)
+
 job_number = int(os.environ['SGE_TASK_ID'])
 decode_string = f'decode_{job_number}.csv'
 params_string = f'params_{job_number}.csv'
 pop_params_string = f'pop_params_{job_number}.csv'
 
+
 def delete_map():
     try:
         os.remove('map_{job_number}.Rdata')
     except FileNotFoundError: 
-        print("Error: couldn't remove the map file, sim probabily failed", file=sys.stderr)
+        print("Error: couldn't remove the map file", file=sys.stderr)
 
 
-def write_params(params, pop_params):
+def write_params(params, pop_params, decode_params):
     # Write params to file
     with open('params.csv', 'r') as file:
         data = file.readlines()
@@ -32,8 +37,7 @@ def write_params(params, pop_params):
     with open(f'{pop_params_string}', 'w') as file:
         file.writelines(data)
         file.write('\n')
-   
-def write_decode_params(decode_params):
+
     with open('decode.csv', 'r') as file:
         data = file.readlines()
 
@@ -43,8 +47,8 @@ def write_decode_params(decode_params):
         file.writelines(data)
         file.write('\n')
 
-        
-def run_sim(decode_params):
+
+def run_sim():
 
     new_env = os.environ.copy()
     new_env["RAPPOR_REPO"] = "../../"
@@ -58,47 +62,47 @@ def run_sim(decode_params):
     results_check = sum([int(x[1]) for x in results_check])
     if results_check == 0:
         return "Signal too poor"
-    results = [results[12],results[14]]
+    results = [results[12], results[14]]
     return (', '.join([': '.join([y.strip() for y in x[1:]]) for x in results]))
 
 
-# "k","h","m","p","q","f"
-# 256,2,8,0.5,0.75,0
-sim_results = []
+def main():
+  
+    sim_results = []
 
-# Initial default params which we will change and pass to the sim
-params = [256,2,8,0.5,0.75,0]
-pop_params = [300, 0.7, 0.3, 0,"Zipf", 10, 0.05, 300000, 0]
-decode_params = [0.001, 0.001, "majority", "any"]
+    # Initial default params which we will change and pass to the sim
+    params = [256, 2, 8, 0.5, 0.75, 0]
+    pop_params = [300, 0.7, 0.3, 0, "Zipf", 10, 0.05, 300000, 0]
+    decode_params = [0.001, 0.001, "majority", "any"]
 
-# Obviously this next bit is completely horrific but it was quick to do
-params_set = itertools.product(filter_sizes, numbers_of_cohorts, populations, samples, primary_threshold_values,secondary_threshold_values, primary_decisions, secondary_decisions)
-params_list = list(params_set)
-job_params_list = np.array_split(params_list, num_jobs)
+    # Create a list of all possible parameter products
+    params_set = itertools.product(filter_sizes, numbers_of_cohorts, primary_threshold_values, secondary_threshold_values, primary_decisions, secondary_decisions, populations, samples)
+    params_list = list(params_set)
 
-job_number = int(os.environ['SGE_TASK_ID'])
+    # Split the list of jobs between the number of tasks
+    job_params_list = np.array_split(params_list, num_jobs)
+    job_number = int(os.environ['SGE_TASK_ID'])
 
-for p in job_params_list[job_number-1]:
-    params[0] = p[0]
-    params[2] = p[1]
-    pop_params[0] = p[2]
-    pop_params[7] = p[3]
-    decode_params[0] = p[4]
-    decode_params[1] = p[5]
-    decode_params[2] = p[6]
-    decode_params[3] = p[7]
-    print(decode_params)
-    # Deletion of the map: things that change the map go above this line
-    delete_map()
-    write_params(params,pop_params)
-    write_decode_params(decode_params) 
+    for p in job_params_list[job_number-1]:
+        params[0] = p[0]
+        params[2] = p[1]
 
-    #decode_params_set = itertools.product(primary_threshold_values, secondary_threshold_values, primary_decisions, secondary_decisions)
-    sim_results.append((run_sim(decode_params), params.copy(), list(decode_params).copy(), pop_params.copy()))
+        pop_params[0] = p[6]
+        pop_params[7] = p[7]
 
-print("disasters, success, filter_size, cohort_size, primary_threshold, secondary_threshold, primary dec, secondary dec, population size, sample size")
-for x in sim_results:
-    print(x[0], end=',')
-    print(str(x[1][0]) + ', ' + str(x[1][2]) + ", " + str(', '.join([str(s) for s in x[2]])) + ', ' + str(x[3][0]) + ', ' + str(x[3][7]))
+        decode_params[0:3] = p[4:7]
+
+        # Write all parameters to the disk before we run simulation
+        write_params(params, pop_params, decode_params)
+
+        sim_results.append((run_sim(), p.copy()))
+        # Map MUST be deleted after every sim
+        delete_map()
+
+        print("disasters, success, filter_size, cohort_size, primary_threshold, secondary_threshold, primary dec, secondary dec, population size, sample size")
+        for (x, y) in sim_results:
+            print(str(x) + ", " + (', '.join([str(s) for s in y])))
 
 
+if __name__ == '__main__':
+    main()
